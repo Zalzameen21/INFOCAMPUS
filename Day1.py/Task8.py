ebd00cec2b442ec33a4311674284624024c8bbf9
Zalzameen21 <zalzameen2002@gmail.com>
2026-01-12 10:22:23 +0530

    Update Task8.py

--- Day1.py/Task8.py
+++ Day1.py/Task8.py
@@ -1,1 +1,7 @@
-
+balance=10000
+amoumt=int(input("Enter amount to withdraw:"))
+if amount <= balance:
+    bal=balance - amount
+    print("amount withdrawn:",amount,",available balance:",bal)
+else:
+    print("Insufficient balance")
