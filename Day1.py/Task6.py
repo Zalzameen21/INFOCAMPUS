71d4c2fd083b47f70cd59301a30b1745d48c2c12
Zalzameen21 <zalzameen2002@gmail.com>
2026-01-12 10:13:03 +0530

    Update Task6.py

--- Day1.py/Task6.py
+++ Day1.py/Task6.py
@@ -1,1 +1,9 @@
-
+marks=int(input("Enter your mark:"))
+if marks>=80:
+    print("Grade = A")
+elif marks>=60:
+    print("Grade = B")
+elif marks>=40:
+    print("Grade = C")
+else:
+    print("Failed")
